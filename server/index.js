b14ccbbdba564e67ff7069dcda65b94345a277ea
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-04-20 20:38:04 -0700

    Created basic dir structure & created hellow word html page.

--- server/index.js
+++ server/index.js
@@ -3,13 +3,18 @@ var port = isDeveloping ? 3000 : process.env.PORT;
 var hostname = process.env.HOSTNAME || '0.0.0.0';
 var express = require('express');
 var app = express();
+var path = require('path');
+var root = path.dirname(__dirname);
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  var options = {
+    root: root
+  };
+  res.sendFile('app/index.html', options);
 })
 
 var server = app.listen(port, hostname, function () {
 
-  console.log("Example app listening at http://localhost:%s", port)
+  console.log('Example app listening at http://localhost:%s', port)
 
 })

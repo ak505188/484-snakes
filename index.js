d1cd3f333ff23a5d2bac98f89fdcb42c50237ac1
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-04-27 01:02:33 -0700

    Some new helper functions for routing/controller

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var game = require('./routes/game.js');
 
 var isDeveloping = process.env.NODE_ENV !== 'production';
 var port = isDeveloping ? 3000 : process.env.PORT;
 var hostname = process.env.HOSTNAME || '0.0.0.0';
 global.root_dir = __dirname;
+var rooms = {};
 
 app.use(express.static(__dirname + '/common'));
 
@@ -16,32 +16,50 @@ app.get('/', function (req, res) {
 })
 
 app.get('/game', function(req, res) {
-  res.send('Game!');
+  res.sendFile('app/lobby.html', { root: global.root_dir });
 });
 
 app.get('/game/*', function(req, res) {
+  console.log(getRoomFromUri(req.url));
   res.sendFile('app/game.html', { root: global.root_dir });
 });
-
-// app.use('/game', game);
 
 server.listen(port, hostname, function () {
   console.log('Listening on port %s. Open http://localhost:%s in browser.', port, port);
 })
 
 io.on('connection', function (socket) {
+  // Update lobby page with total number of people connected
+  io.emit('new connection', { total_client_count: io.engine.clientsCount });
+
+  // Do stuff on disconnect
+  socket.on('disconnect', function() {
+    io.emit('new connection', { total_client_count: io.engine.clientsCount });
+  });
+
+  // IP & port of client
+  // Really useful for keeping track of connected users
+  var client = {
+    remoteAddress: socket.request.connection.remoteAddress,
+    remotePort: socket.request.connection.remotePort
+  }
+  console.log(client);
+
+  // Get room from URI
   var room = getUriFromSocket(socket);
+
   socket.join(room);
-  // console.log(socket);
 
   console.log(socket.id, 'connected');
   io.to(room).emit('status', { client_count: io.sockets.adapter.rooms[room].length });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
 });
 
 function getUriFromSocket(socket) {
   var url = socket.request.headers.referer.split(socket.request.headers.host);
   return url[1];
+}
+
+function getRoomFromUri(url) {
+  url = url.split('/');
+  return (Object.keys(url).length !== 3 ? undefined : url[2]);
 }

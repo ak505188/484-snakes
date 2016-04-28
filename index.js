6155eb0876f8ad7b4740564d2c5d4a9da98c3c07
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-04-28 11:28:52 -0700

    Create game logic works now

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
 
 var isDeveloping = process.env.NODE_ENV !== 'production';
 var port = isDeveloping ? 3000 : process.env.PORT;
 var hostname = process.env.HOSTNAME || '0.0.0.0';
 global.root_dir = __dirname;
+
 var rooms = {};
 
+// Let our html files use files from /common
 app.use(express.static(__dirname + '/common'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', function (req, res) {
-  res.sendFile('app/index.html', { root: global.root_dir });
+  res.sendFile('app/lobby.html', { root: global.root_dir });
 })
 
-app.get('/game', function(req, res) {
-  res.sendFile('app/lobby.html', { root: global.root_dir });
+// TODO: This route will handle creating a game
+app.post('/g', function(req, res) {
+  // console.log(req.body);
+  var response = createRoom(req.body.room, req.body.settings);
+  res.set('Content-Type', 'application/json;charset=UTF-8');
+  res.status(200).send(JSON.stringify(response));
 });
 
-app.get('/game/*', function(req, res) {
+app.get('/g/*', function(req, res) {
   console.log(getRoomFromUri(req.url));
   res.sendFile('app/game.html', { root: global.root_dir });
 });
@@ -35,6 +44,7 @@ io.on('connection', function (socket) {
   // Do stuff on disconnect
   socket.on('disconnect', function() {
     io.emit('new connection', { total_client_count: io.engine.clientsCount });
+    // TODO: Emit new client count to all rooms
   });
 
   // IP & port of client
@@ -48,10 +58,14 @@ io.on('connection', function (socket) {
   // Get room from URI
   var room = getUriFromSocket(socket);
 
-  socket.join(room);
+  socket.on('join_room', function(data) {
+    var room = getUriFromSocket(socket);
+    if (rooms[room] !== undefined) {
+      socket.join(room);
+      io.to(room).emit('status', { client_count: io.sockets.adapter.rooms[room].length });
+    }
+  });
 
-  console.log(socket.id, 'connected');
-  io.to(room).emit('status', { client_count: io.sockets.adapter.rooms[room].length });
 });
 
 function getUriFromSocket(socket) {
@@ -62,4 +76,20 @@ function getUriFromSocket(socket) {
 function getRoomFromUri(url) {
   url = url.split('/');
   return (Object.keys(url).length !== 3 ? undefined : url[2]);
+}
+
+function createRoom(room_name, settings) {
+  var response = {
+    room_name: room_name,
+    exists: false
+  };
+  if (rooms[room_name] === undefined) {
+    rooms[room_name] = {
+      players: {},
+      settings: settings
+    };
+    return response;
+  }
+  response.exists = true;
+  return response;
 }

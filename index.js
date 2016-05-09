c23e6f3643c867ddb3e60b518a3e1684d6b1473d
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-05-08 22:37:08 -0700

    Move socket logic and helper functions to their own files.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var hostname = process.env.HOSTNAME || '0.0.0.0';
 global.root_dir = __dirname;
 
 var rooms = {};
+var socketLogic = require('./server')(io, rooms);
 
 // Let our html files use files from /common
 app.use(express.static(__dirname + '/common'));
@@ -23,7 +24,7 @@ app.get('/', function (req, res) {
 
 // TODO: This route will handle creating a game
 app.post('/g', function(req, res) {
-  var response = createRoom(req.body.room, req.body.settings);
+  var response = lib.createRoom(req.body.room, req.body.settings, rooms);
   res.set('Content-Type', 'application/json;charset=UTF-8');
   res.status(200).send(JSON.stringify(response));
 });
@@ -41,73 +42,3 @@ app.get('/g/*', function(req, res) {
 server.listen(port, hostname, function () {
   console.log('Listening on port %s. Open http://localhost:%s in browser.', port, port);
 });
-
-io.on('connection', function (socket) {
-  // IP & port of client
-  // Really useful for keeping track of connected users
-  var client = {
-    remoteAddress: socket.request.connection.remoteAddress,
-    remotePort: socket.request.connection.remotePort
-  };
-  var room = lib.getRoomFromUri(getUriFromSocket(socket));
-
-  // Do stuff on disconnect
-  socket.on('disconnect', function() {
-    io.emit('new connection', { total_client_count: io.engine.clientsCount });
-    // TODO: Emit new client count to all rooms
-  });
-
-  socket.on('join_room', function(data) {
-    if (rooms[room] !== undefined) {
-      // add player to room
-      // TODO: remove player on disconnect
-      socket.join(room);
-      rooms[room].players.push(client);
-      io.to(room).emit('status', { client_count: io.sockets.adapter.rooms[room].length });
-    }
-  });
-
-  // Update lobby page with total number of people connected
-  io.emit('new connection', {
-    total_client_count: io.engine.clientsCount,
-    currentGames: getCurrentGames()
-  });
-});
-
-function getCurrentGames() {
-  var currentGames = [];
-  Object.keys(rooms).forEach(function(room) {
-    currentGames.push({
-      room: room,
-      playerCount: Object.keys(rooms[room].players).length
-    });
-  });
-
- // Sort currentGames by playercount
- currentGames.sort(function(a, b) {
-   return b.playerCount - a.playerCount;
- });
-
- return currentGames;
-}
-
-function getUriFromSocket(socket) {
-  var url = socket.request.headers.referer.split(socket.request.headers.host);
-  return url[1];
-}
-
-function createRoom(room_name, settings) {
-  var response = {
-    room_name: room_name,
-    exists: false
-  };
-  if (rooms[room_name] === undefined) {
-    rooms[room_name] = {
-      players: [],
-      settings: settings
-    };
-    return response;
-  }
-  response.exists = true;
-  return response;
-}
